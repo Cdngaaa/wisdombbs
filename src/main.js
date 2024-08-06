7fea777b126238cbd3eb2a5c5abee53f54b08e37
Cdngaaa <995252141@qq.com>
2024-08-06 15:52:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueCookies from 'vue-cookies'
 //引入element plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-//我们使用sass 所以这里将base.css 改成base.scss
 import '@/assets/base.scss'
 //图标 图标在附件中
 import '@/assets/icon/iconfont.css'
